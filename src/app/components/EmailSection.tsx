33df7697b56783b1e6527ba1d817e4877a990052
ahammamlho <ahammamlho96@gmail.com>
2023-12-17 23:11:21 +0100

    delete '

--- src/app/components/EmailSection.tsx
+++ src/app/components/EmailSection.tsx
@@ -21,7 +21,7 @@ const EmailSection = () => {
         <section className='grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4'>
             <div>
                 <h5 className='text-xl font-bold text-white my-2'>
-                    Let's Connect
+                    Let s Connect
                 </h5>
                 <p className='text-[#ADB7BE] mb-4 max-w-md'>
                     I am currently looking for new opportunities, my inbox is always open.
